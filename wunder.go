4d7d32c51d9e9dcb796da1ca7706a3190115624d
nicolas314 <nicolas314@gmail.com>
2017-06-23 13:58:28 +0200

    Switched from ip-api.com to ipinfo.io

--- wunder.go
+++ wunder.go
@@ -7,6 +7,7 @@ package main
 
 import (
     "encoding/json"
+    "errors"
     "fmt"
     "html/template"
     "io"
@@ -15,9 +16,11 @@ import (
     "net/http"
     "net/url"
     "os"
+    "os/signal"
     "path"
     "strconv"
     "strings"
+    "syscall"
     "time"
 )
 
@@ -30,13 +33,68 @@ var (
     APIKey  string
 )
 
-// To absorb data from ip-api.com
+// Need to remember how many calls were made to various APIs
+// to avoid overflooding them.
+type Stamp struct {
+    Hits    int             // How many hits recorded over last period
+    Start   time.Time       // Start of last period
+    MaxHits int             // Max hits to observe
+    Period  time.Duration   // Period duration
+}
+
+// Initialize a stamper
+func (ts *Stamp) Init(MaxHits int, Period time.Duration) {
+    ts.MaxHits = MaxHits
+    ts.Period  = Period
+    ts.Start   = time.Now()
+    ts.Hits    = 0
+}
+
+// Register a hit
+// If at least MaxHits have been recorded within the past Period, return
+// False.
+func (ts *Stamp) Hit() bool {
+    // Examine Stamp: if more than Period time has elapsed, reset stamp
+    // and register one hit
+    if time.Since(ts.Start) > ts.Period {
+        ts.Start = time.Now()
+        ts.Hits = 1
+        return true
+    }
+    ts.Hits += 1
+    if ts.Hits > ts.MaxHits {
+        return false
+    }
+    return true
+}
+// Save state to JSON file
+func (ts *Stamp) Save(filename string) {
+    b, _ := json.Marshal(ts)
+    ioutil.WriteFile(filename, b, 0644)
+}
+
+// Restore state from JSON file
+func (ts *Stamp) Load(filename string, MaxHits int, Period time.Duration) {
+    f, err := os.Open(filename)
+    if err!=nil {
+        ts.Init(MaxHits, Period)
+        return
+    }
+    b, err := ioutil.ReadAll(f)
+    f.Close()
+    if err!=nil {
+        ts.Init(MaxHits, Period)
+        return
+    }
+    json.Unmarshal(b, ts)
+}
+
+// To absorb data from ipinfo.io
 type GeoIP struct {
     // Country     string  `json:"country"`
-    CountryCode string  `json:"countryCode"`
+    CountryCode string  `json:"country"`
     City        string  `json:"city"`
-    Lat         float64 `json:"lat"`
-    Lon         float64 `json:"lon"`
+    Loc         string  `json:"loc"`
 }
 
 // To absorb data from Google maps
@@ -51,9 +109,22 @@ type GooglePos struct {
     } `json:"results"`
 }
 
+var APILimits struct {
+    // ipapi   Stamp
+    wunder  Stamp
+    maps    Stamp
+}
+
 // Geoloc an IP address, return city + country
 func Geolocalize(addr string) (geo GeoIP, err error) {
-    resp, err := http.Get("http://ip-api.com/json/" + addr)
+/*
+    if APILimits.ipapi.Hit() != true {
+        fmt.Println("** ip-api usage limit exceeded")
+        err = errors.New("API usage limit exceeded")
+        return
+    }
+*/
+    resp, err := http.Get("http://ipinfo.io/" + addr + "/json")
     if err != nil {
         fmt.Println(err)
         return
@@ -70,6 +141,10 @@ func Geolocalize(addr string) (geo GeoIP, err error) {
 
 // Geoloc with /country/city
 func Position(country, city string) (lat, lon float64, err error) {
+    if APILimits.maps.Hit()!=true {
+        err = errors.New("API limit exceeded")
+        return
+    }
     resp, err := http.Get("http://maps.googleapis.com/maps/api/geocode/"+
                           "json?address="+city+","+country)
     if err != nil {
@@ -171,6 +246,10 @@ func GetCurrentByPos(lat, lon float64) (cw CurrentConditions, err error) {
     log.Println("getting current conditions for", lat, lon)
     url ="http://api.wunderground.com/api/"+APIKey+
          "/conditions/forecast/lang:EN/q/"+ fmt.Sprintf("%.2f,%.2f.json", lat, lon)
+    if APILimits.wunder.Hit()!=true {
+        err = errors.New("API limit exceeded")
+        return
+    }
     resp, err := http.Get(url)
     if err!=nil {
         log.Println(err)
@@ -234,12 +313,16 @@ func GetCurrentByIP(requester string) (cw CurrentConditions, err error) {
         url ="http://api.wunderground.com/api/"+APIKey+
              "/conditions/forecast/lang:EN/q/"+
              "autoip.json?geo_ip="+requester
+
     } else {
         // Provide geoloc to wunderground
         log.Println("getting current conditions (ip-api) for", requester)
         url ="http://api.wunderground.com/api/"+APIKey+
              "/conditions/forecast/lang:EN/q/"+
-             fmt.Sprintf("%.2f,%.2f.json", geo.Lat, geo.Lon)
+             fmt.Sprintf("%s.json", geo.Loc)
+    }
+    if APILimits.wunder.Hit()!=true {
+        return cw, errors.New("API limit exceeded")
     }
     resp, err := http.Get(url)
     if err!=nil {
@@ -287,21 +370,44 @@ func ShowCurrent(w http.ResponseWriter, req * http.Request) {
         // is set with the real requesting IP address.
         incoming = req.Header.Get("X-Real-IP")
     }
-    log.Println("incoming", incoming)
+    log.Println("incoming", incoming, "req", req.URL.Path)
+
+    // Kick bots out
+    if strings.Contains(req.URL.Path, ".php") ||
+        strings.Contains(req.URL.Path, "xml") ||
+        strings.Contains(req.URL.Path, "html") {
+        http.NotFound(w, req)
+        return
+    }
 
     var cw CurrentConditions
     if len(req.URL.Path)>1 {
         // Split request into /country/city
         elems := strings.Split(req.URL.Path[1:], "/")
-        lat, lon, _ := Position(elems[0], elems[1])
+        if len(elems)!=2 {
+            log.Println("malformed req:", req.URL.Path)
+            http.Error(w, "Malformed request: "+req.URL.Path, 400)
+            return
+        }
+        lat, lon, err := Position(elems[0], elems[1])
+        if err!=nil {
+            log.Println("cannot locate:", elems[0], elems[1], err)
+            http.Error(w, err.Error(), 503)
+            return
+        }
         cw, err = GetCurrentByPos(lat, lon)
+        if err!=nil {
+            log.Println(err)
+            http.Error(w, err.Error(), 503)
+            return
+        }
     } else {
         log.Println("req_addr", incoming)
         cw, err = GetCurrentByIP(incoming)
     }
     if err!=nil {
         log.Println(err)
-        http.NotFound(w, req)
+        http.Error(w, err.Error(), 503)
         return
     }
     w.Header().Set("Content-type", "text/html")
@@ -353,6 +459,22 @@ func main() {
     http.HandleFunc("/favicon.ico", Static)
     http.HandleFunc("/static/", Static)
     http.HandleFunc("/", ShowCurrent)
+
+    // Initialize time stamps for API calls
+    // APILimits.ipapi.Load("ts-ipinfo.json", 120, time.Minute)
+    APILimits.wunder.Load("ts-wunder.json", 1, 3*time.Minute)
+    APILimits.maps.Load("ts-maps.json", 1, 1*time.Minute)
+
+    // Install signal handler for interrupts
+    sigs := make(chan os.Signal, 1)
+    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+    go func() {
+        <-sigs
+        // APILimits.ipapi.Save("ts-ipapi.json")
+        APILimits.wunder.Save("ts-wunder.json")
+        APILimits.maps.Save("ts-maps.json")
+        os.Exit(0)
+    }()
 
     fmt.Println("Listening on", addr)
     err := http.ListenAndServe(addr, nil)
